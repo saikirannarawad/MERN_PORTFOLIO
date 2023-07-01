4894324835b8b5455c4db1589e8c9be688506a6d
saikirannarawad <saikirannarawad0@gmail.com>
2023-07-01 20:59:28 +0530

    index updated

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./Store";
 
-const options = {
-  position: positions.BOTTOM_CENTER,
-  timeout: 5000,
-  transition: transitions.SCALE,
-};
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
